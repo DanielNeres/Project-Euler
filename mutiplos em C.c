ab582335d14ef4695b26933d7fc793ed47b97f2c
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2023-12-16 20:00:30 -0300

    mutiplos em C V.2.1

--- mutiplos em C.c
+++ mutiplos em C.c
@@ -1,15 +1,18 @@
 #include <stdio.h>
 
 int main(void){
-    int soma = 0;
-    for(quantidade = 0; soma < 1000; quantidade++){
-        if(quantidade % 3 == 0){
-            printf("%d\n", quantidade);
+    int soma = 0, numero;
+    for(numero = 0; soma < 1000; numero++){
+        if(numero % 3 == 0){
+            printf("%d\n", numero);
+            soma += numero;
         }
-        if(quantidade % 5 == 0){
-            printf("%d\n", quantidade);
+        if(numero % 5 == 0){
+            printf("%d\n", numero);
+            soma += numero;
         }
     }
+    printf("soma = %d", soma);
 
     return 0;
 }
