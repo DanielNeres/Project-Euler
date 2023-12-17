403688c2571bb73feb52e7853bbf2dae703a9036
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2023-12-17 14:52:41 -0300

    mutiplos em C V.3

--- mutiplos em C.c
+++ mutiplos em C.c
@@ -3,11 +3,7 @@
 int main(void){
     int soma = 0, numero;
     for(numero = 0; soma < 1000 - numero; numero++){
-        if(numero % 3 == 0){
-            printf("%d\n", numero);
-            soma += numero;
-        }
-        else if(numero % 5 == 0){
+        if((numero % 3 == 0) || (numero % 5 == 0)){
             printf("%d\n", numero);
             soma += numero;
         }
