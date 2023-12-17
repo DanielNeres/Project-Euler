e23af411fc3610a189fa8370a6a7b24c47d9fddb
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2023-12-17 14:10:42 -0300

    multiplos em python V.3.1

--- multiplos em python.py
+++ multiplos em python.py
@@ -1,12 +1,12 @@
-a = numero = 0; soma = 0
+numero = 0; soma = 0
 
 while soma < 1000:
     if numero % 3 == 0:
         print(numero)
         soma += numero
         
-    if numero % 5 == 0:
+    elif numero % 5 == 0:
         print(numero)
         soma += numero
-    print(soma)
     numero += 1
+print(soma)
