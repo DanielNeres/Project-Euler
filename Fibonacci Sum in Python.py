f305b5c5d1e2c8aaaf2d507d8f9122a56275e21c
Daniel Neres <146374053+DanielNeres@users.noreply.github.com>
2023-12-18 20:06:24 -0300

    Fibonacci Sum in Python V.1

--- Fibonacci Sum in Python.py
+++ Fibonacci Sum in Python.py
@@ -1,12 +1,9 @@
 numeroimprecao = 1; numeroanterior = 1; soma = 0
-	for(;numeroimprecao <= 4000000;){	
-		printf("\n%li", numeroimprecao); 	
-		numeroimprecao += numeroanterior; 
-		numeroanterior = numeroimprecao - numeroanterior;
-		if(numeroimprecao % 2 == 0){
-        		soma += numeroimprecao;
-		}
-	}
-	printf("\nsoma = %d", soma);
-		return 0;
-}
+while numeroimprecao < 4000000:
+    print(numeroimprecao)
+    numeroimprecao += numeroanterior
+    numeroanterior = numeroimprecao - numeroanterior
+    if numeroimprecao % 2 == 0:
+        soma += numeroimprecao
+
+print("soma =", soma);
