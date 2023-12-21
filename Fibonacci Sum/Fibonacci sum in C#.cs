9eced7793908474e7266f760c5b9c714982b6db1
DanielNeres <danielnerres@gmail.com>
2023-12-21 07:29:20 -0300

    Sequencia de Fibonacci V.2.1

--- Fibonacci Sum/Fibonacci sum in C#.cs
+++ Fibonacci Sum/Fibonacci sum in C#.cs
@@ -4,10 +4,10 @@ public class FibonacciSum
 {
 	public static void Main()
 	{
-		unsigned long long int numeroimprecao = 1, numeroanterior = 1, soma = 0;
+		ulong numeroimprecao = 1, numeroanterior = 1, soma = 0;
 		for (; numeroimprecao <= 4000000;)
 		{
-			printf("\n%li", numeroimprecao);
+			Console.WriteLine(numeroimprecao);
 			numeroimprecao += numeroanterior;
 			numeroanterior = numeroimprecao - numeroanterior;
 			if (numeroimprecao % 2 == 0)
@@ -15,6 +15,6 @@ public class FibonacciSum
 				soma += numeroimprecao;
 			}
 		}
-		printf("\nsoma = %d", soma);
+		Console.WriteLine("soma = " + soma);
 	}
 }
