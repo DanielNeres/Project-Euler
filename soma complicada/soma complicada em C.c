9838f359bb8a8970171b5e8e5ca4ecb6f41d4847
DanielNeres <danielnerres@gmail.com>
2023-12-22 14:55:57 -0300

    Soma Complicado V.2

--- soma complicada/soma complicada em C.c
+++ soma complicada/soma complicada em C.c
@@ -4,28 +4,19 @@
 int calcularsomadedivisores(int numero);
 
 int main(){
-    unsigned long contador = 1000000, divisor = 2017, soma, i, somatotal = 0;
-
-    for(; contador > 0; contador--){
-        soma = 0;
-        for(i = 1; i <= contador; i++){
-            if(contador % i == 0){
-                soma += i;
-            }
-        }
-        if(soma % divisor == 0){
-            printf("%lu\n", contador);
-            somatotal += contador;
-        }
+    unsigned long Numero_colosso = 1000000000, soma, i;
+    int divisor = 2017;
+    for(i = divisor - 1; i < Numero_colosso; i++){
+    soma += calcularsomadedivisores(Numero_colosso);
     }
-    printf("soma = %lu\n", somatotal);
+    printf("soma = %lu\n", soma);
     return 0;
 }
 
 int calcularsomadedivisores(int numero){
-
     unsigned long long soma = 0;
     unsigned long i;
+    soma += 1 + numero;
     if(numero % 2 == 0){
         soma += 2;
         soma += numero/2;
