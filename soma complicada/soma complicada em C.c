1f28f47663ffa63e47843f73a4f29ce5e14d280e
DanielNeres <danielnerres@gmail.com>
2023-12-29 11:50:08 -0300

    Soma Complicada Fenix V.1

--- soma complicada/soma complicada em C.c
+++ soma complicada/soma complicada em C.c
@@ -4,18 +4,18 @@
 int calcularsomadedivisores(int numero);
 
 int main(){
-    unsigned long Numero_colosso = 1000000000, soma, i;
+    unsigned long long Numero_colosso = 1000000000, soma = 0, i;
     int divisor = 2017;
     for(i = divisor - 1; i < Numero_colosso; i++){
-    soma += calcularsomadedivisores(Numero_colosso);
+    soma += calcularsomadedivisores(i);
     }
-    printf("soma = %lu\n", soma);
+    printf("soma = %64lu\n", soma);
     return 0;
 }
 
 int calcularsomadedivisores(int numero){
     unsigned long long soma = 0;
-    unsigned long i;
+    unsigned long long i;
     soma += 1 + numero;
     if(numero % 2 == 0){
         soma += 2;
@@ -27,5 +27,6 @@ int calcularsomadedivisores(int numero){
             soma += i;
         }
     }
+    printf("somaLocal = %64lu\n", soma);
     return soma;
 }
